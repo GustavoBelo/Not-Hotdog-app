c977d84ee2eb7715961b695829460b3b959814a0
Gustavo Belo <gustavobelo28@gmail.com>
2021-07-12 18:14:22 -0300

    Added some contrast functionality.

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'SeeFood' do
+  use_frameworks!
+
+  # Pods for SeeFood
+
+pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+
+
+end
--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import ChameleonFramework
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var cameraIcon: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else { fatalError("Could not convert UIImage into CIImage") }
+            detect(image: ciimage)
+            cameraIcon.tintColor = ContrastColorOf((navigationController?.navigationBar.barTintColor)!, returnFlat: true)
+        }
+        imagePicker.dismiss(animated: true)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Loading CoreML Failed.") }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else { fatalError("Model could not process image.") }
+            
+            if let firstResult = results.first, let navBar = self.navigationController?.navigationBar {
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "Hotdog!"
+                    navBar.barTintColor = FlatGreen()
+                } else{
+                    self.navigationItem.title = "Not Hotdog!"
+                    navBar.barTintColor = FlatRed()
+                }
+                navBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: ContrastColorOf(navBar.barTintColor!, returnFlat: true)]
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
+    }
 }
-
